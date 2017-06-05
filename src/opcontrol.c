736995125338356b45830149c4214e17a436d78b
lbayl0721 <baylessluke@gmail.com>
2017-06-05 11:49:23 -0400

    Fixed arm code

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,26 +1,31 @@
 #include "main.h"
+
+static int clamp(int in) { return (abs(in) > 15) ? in : 0; }
+
 void operatorControl() {
   int power;
   int turn;
     while (1) {
-        power = joystickGetAnalog(1, 3); // vertical axis on left joystick
-        turn  = joystickGetAnalog(1, 1); // horizontal axis on left joystick
+        power = clamp(joystickGetAnalog(1, 3)); // vertical axis on left joystick
+        turn  = clamp(joystickGetAnalog(1, 1)); // horizontal axis on left joystick
         chassisSet(power + turn, power - turn);
-        delay(20);
+
         if (buttonIsNewPress(JOY1_8U)) {
           autonomous();
         }
-        if (buttonGetState(JOY1_6U) == 1) {
-            blrsMotorSet(ARM_LEFT, 127, true);
-            blrsMotorSet(ARM_RIGHT, 127, true);
+        if (buttonGetState(JOY1_6U)) {
+            blrsMotorSet(ARM_LEFT, 60, true);
+            blrsMotorSet(ARM_RIGHT, 60, true);
         }
-        else if(buttonGetState(JOY1_6D) == 1) {
-          blrsMotorSet(ARM_LEFT, -127, true);
-          blrsMotorSet(ARM_RIGHT, -127, true);
+        else if(buttonGetState(JOY1_6D)) {
+          blrsMotorSet(ARM_LEFT, -40, true);
+          blrsMotorSet(ARM_RIGHT, -40, true);
         }
-        else
-        blrsMotorSet(ARM_LEFT, 0, true);
-        blrsMotorSet(ARM_RIGHT, 0, true);
+        else {
+          blrsMotorSet(ARM_LEFT, 0, true);
+          blrsMotorSet(ARM_RIGHT, 0, true);
+        }
 
+        delay(20);
     }
 }
