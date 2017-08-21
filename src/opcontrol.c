7740cd89ddf6bc62fe8c2729b56fc8f1e65e44f4
Unknown <willanderson18@cchsindy.org>
2017-08-21 11:42:45 -0400

    titled code and change chain value & claw

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -24,50 +24,50 @@ void operatorControl() {
 //        }
 
         if(buttonGetState(JOY1_5U)) {
-          blrsMotorSet(ARM_LEFT, -25, true);
+          blrsMotorSet(ARM_LEFT, -25, true);  //Lower Arm
           blrsMotorSet(ARM_RIGHT, -25, true);
         }
-        else if(buttonGetState(JOY1_6U)) {
+        else if(buttonGetState(JOY1_6U)) {  //Raise Arm
           blrsMotorSet(ARM_LEFT, 80, true);
           blrsMotorSet(ARM_RIGHT, 80, true);
         }
         else {
-          blrsMotorSet(ARM_LEFT, 0, true);
+          blrsMotorSet(ARM_LEFT, 0, true);  //Arm =0
           blrsMotorSet(ARM_RIGHT, 0, true);
         }
-        if (buttonGetState(JOY1_6D)) {
+        if (buttonGetState(JOY1_6D)) {  //Going out Intake
           blrsMotorSet(INTAKE, 80, true);
         }
-        else if(buttonGetState(JOY1_5D)) {
+        else if(buttonGetState(JOY1_5D)) {  //Bringing In Intake
           blrsMotorSet(INTAKE, -80, true);
         }
         else {
-          blrsMotorSet(INTAKE, 0, true);
+          blrsMotorSet(INTAKE, 0, true);  //Intake =0
         }
 
-        if(buttonGetState(JOY1_7U)) {
-          blrsMotorSet(CHAIN, 50, true);
-          blrsMotorSet(CHAIN, 50, true);
+        if(buttonGetState(JOY1_7U)) {   //Raise chain
+          blrsMotorSet(CHAIN, 60, true);
+    //      blrsMotorSet(CHAIN, 60, true);
         }
-        else if(buttonGetState(JOY1_7D)){
-          blrsMotorSet(CHAIN, -40, true);
-          blrsMotorSet(CHAIN, -40, true);
-        }
-        else{
-          blrsMotorSet(CHAIN, 0, true);
-          blrsMotorSet(CHAIN, 0, true);
-        }
-        if(buttonGetState(JOY1_8U)){
-          blrsMotorSet(CLAW, 60, true);
-          blrsMotorSet(CLAW, 60, true);
-        }
-        else if(buttonGetState(JOY1_8D)){
-          blrsMotorSet(CLAW, -60, true);
-          blrsMotorSet(CLAW, -60, true);
+        else if(buttonGetState(JOY1_7D)){ //Lower chain
+          blrsMotorSet(CHAIN, -35, true);
+  //        blrsMotorSet(CHAIN, -35, true);
         }
         else{
-          blrsMotorSet(CLAW, 0, true);
-          blrsMotorSet(CLAW, 0, true);
+          blrsMotorSet(CHAIN, 0, true); //chain =0
+  //        blrsMotorSet(CHAIN, 0, true);
+        }
+        if(buttonGetState(JOY1_8U)){  //Open Claw
+          blrsMotorSet(CLAW, 80, true);
+  //        blrsMotorSet(CLAW, 80, true);
+        }
+        else if(buttonGetState(JOY1_8D)){ //Close claw
+          blrsMotorSet(CLAW, -60, true);
+  //        blrsMotorSet(CLAW, -60, true);
+        }
+        else{
+          blrsMotorSet(CLAW, 0, true);  //Claw =0
+  //        blrsMotorSet(CLAW, 0, true);
         }
         delay(20);
     }
