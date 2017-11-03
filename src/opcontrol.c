ff659dd8e0a56149055681704de2c7a91ebec437
lbayl0721 <baylessluke@gmail.com>
2017-11-03 13:55:34 -0400

    slight commenting changes

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -15,7 +15,7 @@ while (1) {                                       //start of opcontrol while loo
 //  lcdPrint(uart1, 1, "encoder: %d", chassisLeftPos() );
   lcdPrint(uart1, 1, "battery %d", powerLevelMain());
   lcdPrint(uart1, 2, "mogo pot %d", analogRead(MOGO_POT));
-  mogoTasksInit(); //initialize the tasks
+//   mogoTasksInit(); //initialize the tasks
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int power = clamp(joystickGetAnalog(1, 3)); // vertical axis on left joystick
 int turn  = clamp(joystickGetAnalog(1, 4)); // horizontal axis on right joystick
@@ -25,7 +25,7 @@ chassisSet(power + turn, power - turn);
   arm_state = MANUAL;
   blrsMotorSet(CHAIN, 80, true);
 }
-else if(buttonGetState(JOY1_6D) && analogRead(CHAIN_POT) > 10){       //Button 6D for manual in control of Chain bar
+else if(buttonGetState(JOY1_6D) && analogRead(CHAIN_POT) > 12){       //Button 6D for manual in control of Chain bar
   arm_state = MANUAL;
   blrsMotorSet(CHAIN, -60, true);
 }
@@ -35,7 +35,7 @@ else if(arm_state == MANUAL){
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if(buttonGetState(JOY1_5D)) {         //Button 5D for lowering arm
   arm_state = MANUAL;
-  liftSet(-100);
+  liftSet(-70);
 }
 else if(buttonGetState(JOY1_5U)) {   //Button 5U for raising Arm
   arm_state = MANUAL;
@@ -55,16 +55,16 @@ else if (arm_state == MANUAL)           //suspends autoReturnTask if in manual c
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //if (buttonIsNewPress(JOY1_7U) && analogRead(ARM_POT) < 1400){
 if (buttonGetState(JOY1_7U) && analogRead(ARM_POT) < 1400) {      //Button 7U for going out mogo intake
-//  blrsMotorSet(INTAKE, 80, true);
+  blrsMotorSet(INTAKE, 80, true);
 //  mogoSetPos(800);
-  mogoOutResume();
-  mogoInSuspend();
+//  mogoOutResume();
+//  mogoInSuspend();
 }
 else if(buttonGetState(JOY1_7D) && analogRead(ARM_POT) < 1400) {  //Button 7D for bringing in mogo intake
-//  blrsMotorSet(INTAKE, -80, true);
-//  mogoSetPos(2400);
-    mogoInResume();
-    mogoOutSuspend();
+  blrsMotorSet(INTAKE, -80, true);
+//    mogoSetPos(2400);
+//    mogoInResume();
+//    mogoOutSuspend();
 }
 else {
   blrsMotorSet(INTAKE, 0, true);    //Intake =0
@@ -74,7 +74,7 @@ else {
 if(buttonGetState(JOY1_8D)){   //Button 8D for open claw
   blrsMotorSet(CLAW, -100, true);
   }
-else if(buttonGetState(JOY1_8U)){
+else if(buttonGetState(JOY1_8U)){ //Button 8U for close claw
   blrsMotorSet(CLAW, 100, true);
 }
 else{
